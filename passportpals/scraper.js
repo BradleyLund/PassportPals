c21a86e4793efd289558a6a005389d6fca80dcb9
Bradley Lund <bradjlund@gmail.com>
2020-07-20 12:49:50 +0200

    scrape south africa

--- passportpals/scraper.js
+++ passportpals/scraper.js
@@ -1,1 +1,15 @@
 const puppeteer = require('puppeteer')
+
+async function scrapeChannel(url) {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
+
+    const [el] = await page.$x('/html/body/div[3]/div[3]/div[5]/div/table[1]/tbody')
+    const text = await el.getProperty('innerHTML')
+    console.log(text)
+
+    browser.close();
+}
+
+scrapeChannel('https://en.wikipedia.org/wiki/Visa_requirements_for_South_African_citizens')
