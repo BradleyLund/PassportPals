d55f4aa42e32f58f0bc0e9d56fcbbc0163059efe
Bradley Lund <bradjlund@gmail.com>
2020-07-28 15:47:46 +0200

    Scrape second page of URL's

--- passportpals/scrapeCountryURL.js
+++ passportpals/scrapeCountryURL.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 const scraper = require('./scraper')
 
-async function scrapeURLs(url) {
+async function scrapeURLs(url,url2) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto(url)
@@ -12,15 +12,29 @@ async function scrapeURLs(url) {
     const data = await page.evaluate(()=> {
         const group = document.querySelector('div.mw-category')
         
+        
         const anchors = Array.from(group.querySelectorAll('a'))
         return anchors.map(a=>a.href)
     })
 
+    //scrape the URL's from page 2
+
+    await page.goto(url2)
+
+    const data2 = await page.evaluate(()=> {
+        const group2 = document.querySelector('div.mw-category')
+
+        const anchors2 = Array.from(group2.querySelectorAll('a'))
+        return anchors2.map(a=>a.href)
+    })
+
+    const allURLs = [...data,...data2]
+
     //console.log(data)
-    for (let country of data) {
-       await scraper.scrapeCountry(country)
-    }
+    // for (let country of data) {
+    //    await scraper.scrapeCountry(country)
+    // }
     browser.close()
 }
 
-scrapeURLs('https://en.wikipedia.org/wiki/Category:Visa_requirements_by_nationality')
+scrapeURLs('https://en.wikipedia.org/wiki/Category:Visa_requirements_by_nationality','https://en.wikipedia.org/w/index.php?title=Category:Visa_requirements_by_nationality&pagefrom=Trinidad+and+Tobago%0AVisa+requirements+for+Trinidad+and+Tobago+citizens#mw-pages')
