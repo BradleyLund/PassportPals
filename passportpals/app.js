5ff2bfe54e1ac1a55b062940b2525ca462f24c34
Bradley Lund <bradjlund@gmail.com>
2020-07-30 20:23:09 +0200

    what

--- passportpals/app.js
+++ passportpals/app.js
@@ -3,15 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config({path: __dirname + '/.env'})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var steven = require('./routes/steven')
 var mongoose = require('mongoose')
 
-var mongoDB =  'mongodb+srv://badgerb:ToljJo0eTWzY5YNj@cluster0.d0llu.mongodb.net/test?retryWrites=true&w=majority'
+var mongoDB =  process.env.DB_URI
 
-mongoose.connect(mongoDB, { useNewUrlParser: true })
+mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true })
 var db = mongoose.connection
 db.on('error',console.error.bind(console,'MongoDB connection error:'))
 
@@ -35,6 +36,30 @@ app.use('/steven',steven) //maybe use this for individual country
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+var Schema = mongoose.Schema
+
+var SubSchema = mongoose.Schema({
+    //your subschema content
+    description: String,
+    duration: Number,
+    date: String 
+},{ _id : false });
+
+
+
+var UserSchema = new Schema({
+  username: String,
+  exercise: [SubSchema]
+  
+})
+
+var User = mongoose.model('User',UserSchema)
+
+User.findOne({},function(err,user) {
+  if (err) {console.log(error)}
+  console.log(user)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
--- passportpals/models/country.js
+++ passportpals/models/country.js
@@ -12,3 +12,14 @@ var CountrySchema = new Schema(
         other: [{type: String}]
     }
 )
+
+
+//virtual for the individual country url to display each countries data individually
+CountrySchema
+.virtual('url')
+.get(function() {
+    return '/individualCountry/'+this.origin_name //hopefully this works, might be issue with spaces in names
+})
+
+//export the model
+module.exports = mongoose.model('Country',CountrySchema)
