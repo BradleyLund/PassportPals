8d957d7ec0293d9f4d5b029fd983dba2ed9e4fe8
Bradley Lund <bradjlund@gmail.com>
2020-08-01 17:27:29 +0200

    Big scrape

--- passportpals/scraper.js
+++ passportpals/scraper.js
@@ -158,6 +158,6 @@ module.exports = {
 
 
 
-scrapeCountry('https://en.wikipedia.org/wiki/Visa_requirements_for_South_African_citizens')
+//scrapeCountry('https://en.wikipedia.org/wiki/Visa_requirements_for_South_African_citizens')
 
 
