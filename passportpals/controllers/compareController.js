68d01f16e59ebe978768044f77ee58c164a7a1fd
Bradley Lund <bradjlund@gmail.com>
2020-08-04 09:06:31 +0200

    display drop downs with countries

--- passportpals/controllers/compareController.js
+++ passportpals/controllers/compareController.js
@@ -2,7 +2,14 @@ var Country = require('../models/country')
 var async = require('async')
 
 const validator = require('express-validator')
+const { count } = require('../models/country')
 
 exports.compareCountry_get = function(req,res,next) {
     //get all countries and then send them to the view
+
+    //find all countries and then select only the origin_name?
+    Country.find({},function(err,countries) {
+        if (err) {return next(err)}
+        res.render('compareCountry',{ title: 'Compare Countries', countries: countries})
+    })
 }
