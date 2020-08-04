1e829799fbe6f2456b8a1f8efe8e57a11023127e
Bradley Lund <bradjlund@gmail.com>
2020-08-04 13:16:42 +0200

    first compare function

--- passportpals/controllers/compareController.js
+++ passportpals/controllers/compareController.js
@@ -13,3 +13,32 @@ exports.compareCountry_get = function(req,res,next) {
         res.render('compareCountry',{ title: 'Compare Countries', countries: countries})
     })
 }
+
+exports.compareTwo = function(req,res,next) {
+    async.parallel({
+        country1: function(callback) {
+            Country.findOne({origin_name: `${req.query.country}`},callback)
+        },
+        country2: function(callback) {
+            Country.findOne({origin_name: `${req.query.country2}`},callback)
+        },
+    }, function(err,results) {
+        if (err) {return next(err)}
+        //compare the two countries and make a new country object to send to a render page
+        let combinedVisaRequired = []
+        let combindeNoVisaRequired = []
+        let combinedeVisa = []
+        let combinedOnArrival = []
+        let combinedOther = []
+
+        for (let i=0;i<results.country1.no_visa_required.length;i++) {
+            if (results.country2.no_visa_required.indexOf(results.country1.no_visa_required[i])!=-1){
+                combindeNoVisaRequired.push(results.country1.no_visa_required[i])
+            }
+        }
+
+        res.json(combindeNoVisaRequired)
+    })
+    //find the two countries in the database and then display their stuff
+    //just like the individual country one
+}
--- passportpals/routes/compareCountry.js
+++ passportpals/routes/compareCountry.js
@@ -5,11 +5,6 @@ var compareController = require('../controllers/compareController')
 
 router.get('/',compareController.compareCountry_get)
 
-router.get('/comp',function(req,res) {
-    res.send(`${req.query.country} ${req.query.country2}`)
-
-    //find the two countries in the database and then display their stuff
-    //just like the individual country one
-})
+router.get('/comp',compareController.compareTwo)
 
 module.exports = router
