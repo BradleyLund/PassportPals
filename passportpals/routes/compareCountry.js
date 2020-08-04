a2915c42a68ee7fb0a8124dbf6e2d85a12ae9980
Bradley Lund <bradjlund@gmail.com>
2020-08-04 09:31:33 +0200

    add query paramaters and action=comp

--- passportpals/routes/compareCountry.js
+++ passportpals/routes/compareCountry.js
@@ -5,4 +5,11 @@ var compareController = require('../controllers/compareController')
 
 router.get('/',compareController.compareCountry_get)
 
+router.get('/comp',function(req,res) {
+    res.send(`${req.query.country} ${req.query.country2}`)
+
+    //find the two countries in the database and then display their stuff
+    //just like the individual country one
+})
+
 module.exports = router
