f26e27a4ef0635a024ceca8497aa884c4344898f
Bradley Lund <bradjlund@gmail.com>
2020-08-05 08:33:17 +0200

    simple visa compare

--- passportpals/controllers/compareController.js
+++ passportpals/controllers/compareController.js
@@ -30,18 +30,49 @@ exports.compareTwo = function(req,res,next) {
         let combinedeVisa = []
         let combinedOnArrival = []
         let combinedOther = []
+        let allCountries = []
 
         for (let i=0;i<results.country1.no_visa_required.length;i++) {
             if (results.country2.no_visa_required.indexOf(results.country1.no_visa_required[i])!=-1){
                 combinedNoVisaRequired.push(results.country1.no_visa_required[i])
             } 
+            allCountries.push(results.country1.no_visa_required[i])
         }
 
-        results.country1.visa_required.map((country)=>{
+        for (let i=0;i<results.country1.visa_required.length;i++) {
+            if (results.country2.visa_required.indexOf(results.country1.visa_required[i])!=-1){
+                combinedVisaRequired.push(results.country1.visa_required[i])
+            } 
+            allCountries.push(results.country1.visa_required[i])
+        }
 
-        })
+        for (let i=0;i<results.country1.eVisa.length;i++) {
+            if (results.country2.eVisa.indexOf(results.country1.eVisa[i])!=-1){
+                combinedeVisa.push(results.country1.eVisa[i])
+            } 
+            allCountries.push(results.country1.eVisa[i])
+        }
 
-        res.json(combinedNoVisaRequired)
+        for (let i=0;i<results.country1.visa_on_arrival.length;i++) {
+            if (results.country2.visa_on_arrival.indexOf(results.country1.visa_on_arrival[i])!=-1){
+                combinedOnArrival.push(results.country1.visa_on_arrival[i])
+            } 
+            allCountries.push(results.country1.visa_on_arrival[i])
+        }
+
+        for (let i=0;i<results.country1.other.length;i++) {
+            if (results.country2.other.indexOf(results.country1.other[i])!=-1){
+                combinedOther.push(results.country1.other[i])
+            } 
+            //allCountries.push(results.other[i])
+        }
+
+        let data = {no_visa_required: combinedNoVisaRequired, visa_required: combinedVisaRequired,eVisa: combinedeVisa,visa_on_arrival: combinedOnArrival,other: combinedOther}
+
+
+
+
+        res.render('combined-view',{title: `${results.country1.origin_name} and ${results.country2.origin_name}`,data: data})
     })
     //find the two countries in the database and then display their stuff
     //just like the individual country one
