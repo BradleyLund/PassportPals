01500743b832fa44549c2476accbf1b71fec390b
Bradley Lund <bradjlund@gmail.com>
2020-08-07 09:59:36 +0200

    styling and routing changes

--- passportpals/controllers/compareController.js
+++ passportpals/controllers/compareController.js
@@ -6,7 +6,7 @@ const { count } = require('../models/country')
 
 exports.compareCountry_get = function(req,res,next) {
     //get all countries and then send them to the view
-
+    
     //find all countries and then select only the origin_name?
     Country.find({},function(err,countries) {
         if (err) {return next(err)}
